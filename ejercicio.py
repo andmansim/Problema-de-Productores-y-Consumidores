227881723eff3ed885677e056f4037d180a831d8
andmansim <91722324+andmansim@users.noreply.github.com>
2023-04-05 10:40:21 +0200

    comentarios

--- ejercicio.py
+++ ejercicio.py
@@ -4,15 +4,24 @@ import time
 
 #creamos la cola que almacena los recursos
 q = Queue(10)
+
+#Creamos el productor
 def productor(nombre):
-    contar= 1
+    '''
+    Añade los recursos que crea el productor a la cola
+    '''
+    contar = 1 
     while True:
         q.join()
         q.put(contar)
         print(f'{nombre} está produciendo el recurso {contar}')
         contar +=1
-
+        
+#Creamos el consumidor
 def consumidor(nombre):    
+    '''
+    Coge un recurso de la cola
+    '''
     contar = 1
     while True:
         a = q.get()
@@ -22,7 +31,10 @@ def consumidor(nombre):
         time.sleep(1)
         
 if __name__ == '__main__':
-    t = Thread(target = productor, args = 'Patata')
-    t1 = Thread(target = consumidor, args= 'Xiao')
+    '''
+    Creamos los hilos de ambas funciones para que se ejecuten en paralelo
+    '''
+    t = Thread(target = productor, args = ('Productor', ))
+    t1 = Thread(target = consumidor, args= ('Consumidor', ))
     t.start()
     t1.start()    
