82c0b26bdfe8263cb8c7e8fff9fc48ca4cb0428d
andmansim <91722324+andmansim@users.noreply.github.com>
2023-04-05 10:17:28 +0200

    productor

--- ejercicio.py
+++ ejercicio.py
@@ -1,3 +1,13 @@
 from queue import Queue
 from threading import Thread
 import time
+
+#creamos la cola que almacena los recursos
+q = Queue(10)
+def productor(nombre):
+    contar= 1
+    while True:
+        q.join()
+        q.put(contar)
+        print(f'{nombre} está produciendo el recurso {contar}')
+        contar +=1
